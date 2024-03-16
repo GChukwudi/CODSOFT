0d0d41045350654e3eeda13b5323a2b5013b394d
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-16 19:52:01 +0200

    updated

--- application/models/userModel.js
+++ application/models/userModel.js
@@ -47,4 +47,9 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, 10);
 });
 
+// Compare user password
+userSchema.methods.comparePassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model('User', userSchema);
