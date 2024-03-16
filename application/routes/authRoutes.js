003fc68d55294aca2f3c851618ba149855dfdb81
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-16 20:34:11 +0200

    updated

--- application/routes/authRoutes.js
+++ application/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { signup } = require('../controllers/authcontrollers');
+const { signup, signin } = require('../controllers/authcontrollers');
 const router = express.Router();
 
 // auth routes
 // /api/signup
 router.post('/signup', signup);
+// /api/signin
+router.post('/signin', signin);
 
 module.exports = router;
