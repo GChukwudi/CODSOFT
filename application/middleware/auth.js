ccf66e96c02220267c5efa1db37872077d664283
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-16 21:25:51 +0200

    updated

--- application/middleware/auth.js
+++ application/middleware/auth.js
@@ -5,4 +5,6 @@ const User = require('../models/userModel');
 // check if user is authenticated
 exports.isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
+
+    if (!token) {}
 }
