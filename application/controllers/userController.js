958ded8e77f41289f39a49cb521b4821cd4e99f1
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-16 21:50:14 +0200

    updated

--- application/controllers/userController.js
+++ application/controllers/userController.js
@@ -3,8 +3,14 @@ const ErrorResponse = require('../utils/errorResponse');
 
 // load all users
 exports.allUsers = async (req, res, next) => {
+
+    // enable pagination
+    const pageSize = 10;
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await User.find({}).estimatedDocumentCount();
+
     try {
-        const users = await User.find();
+        const users = await User.find().sort({ createdAt });
         res.status(200).json({
             success: true,
             users
