86edec8c9e936e6b8cf84fd8974f47c0b158511e
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-16 21:25:35 +0200

    updated

--- application/middleware/auth.js
+++ application/middleware/auth.js
@@ -2,4 +2,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
-// 
+// check if user is authenticated
+exports.isAuthenticated = async (req, res, next) => {
+    const { token } = req.cookies;
+}
