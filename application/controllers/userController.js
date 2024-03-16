b71869f8cdcfdfd763e7c3627abcb6c5bc7341da
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-16 21:47:30 +0200

    updated

--- application/controllers/userController.js
+++ application/controllers/userController.js
@@ -1,3 +1,15 @@
 const User = require('../models/userModel');
 const ErrorResponse = require('../utils/errorResponse');
 
+// load all users
+exports.allUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({
+            success: true,
+            users
+        });
+    } catch (error) {
+        next(error);
+    }
+}
