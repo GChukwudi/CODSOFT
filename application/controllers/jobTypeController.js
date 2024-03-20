ecb980fd3d3159b89c4b8886ac887906636d170c
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-20 07:54:07 +0200

    updated

--- application/controllers/jobTypeController.js
+++ application/controllers/jobTypeController.js
@@ -1,12 +1,12 @@
-const jobType = require('../models/jobTypeModel');
+const createJobTypeobType = require('../models/jobTypeModel');
 const ErrorResponse = require('../utils/errorResponse');
 
 
 // create job category
 exports.createJobType = async (req, res, next) => {
     try {
-        const jobT = await jobType.create({
-            JobTypeName: req.body.JobTypeName,
+        const jobT = await JobType.create({
+            jobTypeName: req.body.jobTypeName,
             user: req.user.id
         });
         res.status(201).json({
