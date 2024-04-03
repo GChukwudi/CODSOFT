55f8c9cbfea56efe494ba827b88c84df252c414e
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-03 17:46:05 +0200

    updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
-import { Box } from "@mui/system";
+import { Box, Container, Stack, useTheme } from "@mui/system";
 
 const Home = () => {
     const { palette } = useTheme();
