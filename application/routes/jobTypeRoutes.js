a1e8ae85a60de27dabf140e32e8a0801fc74194c
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-03 09:25:48 +0200

    updated

--- application/routes/jobTypeRoutes.js
+++ application/routes/jobTypeRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { createJobType, allJobTypes } = require('../controllers/jobTypeController');
-const { isAuthenticated } = require('../middleware/auth');
+const { createJobType, allJobTypes, updateJobType } = require('../controllers/jobTypeController');
+const { isAuthenticated, isAdmin } = require('../middleware/auth');
 
 
 // jobType routes
 
 // /api/type/create
-router.post('/type/create', isAuthenticated, createJobType)
+router.post('/type/create', isAuthenticated, isAdmin, createJobType)
 
 // /api/type/jobs
 router.get('/type/jobs', allJobTypes)
+// /api/type/update/:id
+router.put('/type/update/:id', isAuthenticated, isAdmin, updateJobType)
 
 module.exports = router;
