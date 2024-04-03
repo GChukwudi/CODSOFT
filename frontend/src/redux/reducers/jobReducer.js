0bd537a5dc03adc6b677f6512d04104b09a1da91
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-03 12:52:52 +0200

    updated

---
+++ frontend/src/redux/reducers/jobReducer.js
@@ -0,0 +1,31 @@
+
+
+
+
+export const loadJobReducer = (state = { jobs: [] }, action) => {
+    switch (action.type) {
+        case LOAD_JOBS:
+            return { loading: true }
+        case LOAD_JOBS_SUCCESS:
+            return {
+                loading: true,
+                success: action.payload.success,
+                page: action.payload.page,
+                pages: action.payload.pages,
+                count: action.payload.count,
+                setUniqueLocations: action.payload.setUniqueLocations,
+                jobs: action.payload.jobs
+            }
+        case LOAD_JOBS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case LOAD_JOBS_RESET:
+            return {}
+
+        default:
+            return state;
+    }
+}
+
